46e6bec5b6b33f0e9482068126f5f6dab8d19de4
Abbi Devins-Suresh <aesther2012@gmail.com>
2024-05-16 09:21:55 -0400

    fixed errors

--- elevator.py
+++ elevator.py
@@ -8,11 +8,15 @@ from statemachine.exceptions import InvalidDefinition
 # https://pypi.org/project/python-statemachine/
 
 class Elevator:
-    def __init__(self, num_floors=2, door_open=False, max_occupancy=10, door_closed=False):
-        self.door_open = door_open
-        self.door_closed = door_closed
+    def __init__(self, num_floors, max_occupancy=10, elevator_available=False, transport_active=False):
+        self.door_open = False
+        self.door_closed = False
         self.num_floors = num_floors
         self.max_occupancy = max_occupancy
+        self.current_floor = 1
+        self.elevator_available = elevator_available
+        self.transport_active = transport_active
+        super(Elevator, self).__init__()
 
     def push_button(self):
         self.open_doors()
@@ -25,12 +29,18 @@ class Elevator:
         self.door_open = False
         self.door_closed = True
 
+    def transport(self, end_floor):
+        self.transport_active = True
+        self.elevator_available = False
+        self.current_floor = end_floor
+
 
 class ElevatorControl(StateMachine):
+
     # set states of the elevator
     idle_state = State(initial=True, enter="push_button")
     button_pushed = State()
-    # internal button too
+    # floor_button = State()
     opening_gates = State()
     transporting = State()
     closing_gates = State()
@@ -42,27 +52,16 @@ class ElevatorControl(StateMachine):
             | button_pushed.to(button_pushed, unless="door_open")
     )
     transporting_persons = (
-            opening_gates.to(transporting, cond="transport")
-            | opening_gates.to(opening_gates, unless="transport")
+            opening_gates.to(transporting, cond="transport_active")
+            | opening_gates.to(opening_gates, unless="transport_active")
     )
     finishing_transport = transporting.to(closing_gates, cond="door_closed")
     completed_transport = closing_gates.to(finished_trip)
 
-    def __init__(self, elevator_available=False, transport_active=False):
-        self.current_floor = 1
-        self.elevator_available = elevator_available
-        self.transport_active = transport_active
-        super(ElevatorControl, self).__init__()
-
-    def transport(self, end_floor):
-        self.transport_active = True
-        self.elevator_available = False
-        self.current_floor = end_floor
-
 
 # instantiate elevator classes
 new_elevator = Elevator(num_floors=2)
-new_elevator_control = ElevatorControl(elevator_available=False)
+new_elevator_control = ElevatorControl(new_elevator)
 
 # run elevator
 new_elevator_control.elevator_available = False
